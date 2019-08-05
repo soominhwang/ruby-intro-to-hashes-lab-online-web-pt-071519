f8cd86799a71711ef56b2651ad96ef49c3992ac5
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-05 04:26:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,14 +67,6 @@ def monopoly_with_fourth_tier
         {
           "mortage_value" => "$800"
         }
-      },
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200,
       }
-    }
-  }
 
 end
